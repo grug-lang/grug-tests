425c7d38de161ed4bcac8531460d4f681a511f4f
Sander Bos <sander@streamhpc.com>
2025-11-10 19:06:27 +0100

    fix: link against libmath.so

--- smoketest.c
+++ smoketest.c
@@ -1,7 +1,7 @@
 #include "tests.h"
 
 // TODO: Update tests.sh to run this, where clang instead of gcc must still be pickable too:
-// gcc smoketest.c tests.c -Wall -Wextra -pedantic -Werror -Wfatal-errors -fsanitize=address,undefined -g
+// gcc smoketest.c tests.c -Wall -Wextra -pedantic -Werror -Wfatal-errors -fsanitize=address,undefined -g -lm
 
 // TODO: Call the functions in tests.h.
 int main(void) {

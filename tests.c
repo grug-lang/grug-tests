14e8087a67dbdbc995fdbf6963c1ea7f4d5580a6
Sander Bos <sander@streamhpc.com>
2026-04-06 17:45:49 +0200

    tests: add tests/err/local_variable_contains_entity

--- tests.c
+++ tests.c
@@ -3448,6 +3448,7 @@ static void add_error_tests(void) {
 	ADD_TEST_ERROR(invalid_end_of_block, "D");
 	ADD_TEST_ERROR(line_continuation, "A");
 	ADD_TEST_ERROR(local_variable_already_exists, "D");
+	ADD_TEST_ERROR(local_variable_contains_entity, "D");
 	ADD_TEST_ERROR(local_variable_definition_cant_use_itself, "D");
 	ADD_TEST_ERROR(local_variable_definition_missing_type, "D");
 	ADD_TEST_ERROR(max_expr_recursion_depth_exceeded, "D");
